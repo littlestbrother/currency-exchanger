0174c0ee552ff4d39dae51cbba6c7463e5cbcdd0
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-08-14 19:40:19 -0700

    successfully call with async function

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from './exchange'
 $(console.log(`${process.env.KEY}`));
+
+$(document).ready(function () {
+    $("button").click(function () {
+        console.log(Exchange.getExchangeRate());
+    });
+});

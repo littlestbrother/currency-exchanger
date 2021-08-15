0f5938e1a4db23025eb24d81b80e3e217dbd5ca7
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-08-14 20:26:10 -0700

    use error handling... i think this counts?

--- src/exchange.js
+++ src/exchange.js
@@ -1,13 +1,13 @@
 export default class Exchange {
     static async getExchangeRate() {
         try {
-            const response = await fetch(`https://v6.exchangerate-api.com/v6/7c58c4e8a9a9f439f2919ddc/pair/EUR/GBP`);
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.KEY}/pair/EUR/GBP`);
             if (!response.ok) {
                 throw Error(response.statusText);
             }
             return response.json();
         } catch (error) {
-            return error.message;
+            return error;
         }
     }
 } 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './exchange'
-$(console.log(`${process.env.KEY}`));
+// $(console.log(`${process.env.KEY}`));
+
+async function getResults(response){
+    if(response.result=="success"){
+        console.log(`success! ${response}`);
+    } else {
+        console.log(`failure! ${response}`)
+    }
+}
 
 $(document).ready(function () {
     $("button").click(function () {
-        console.log(Exchange.getExchangeRate());
+        Exchange.getExchangeRate().then(function(response){
+            getResults(response)
+        });
+
     });
 });
